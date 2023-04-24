9a2e61df8a74498c7aa0311d0bcd9d1439f0edaa
WolfyZA <48529875+WolfyZA@users.noreply.github.com>
2023-04-24 19:23:36 +0200

    Update utils.c

--- utils.c
+++ utils.c
@@ -54,15 +54,15 @@ return (0);
 /**
 * convert_size_number - Casts a number to the specified size
 * @num: Number to be casted.
-* @size: Number indicating the type to be casted.
+* @sze: Number indicating the type to be casted.
 *
 * Return: Casted value of num
 */
-long int convert_size_number(long int num, int size)
+long int convert_size_number(long int num, int sze)
 {
-if (size == S_LONG)
+if (sze == S_LONG)
 return (num);
-else if (size == S_SHORT)
+else if (sze == S_SHORT)
 return ((short)num);
 
 return ((int)num);
@@ -71,13 +71,13 @@ return ((int)num);
 /**
 * convert_size_unsgnd - Casts a number to the specified size
 * @num: Number to be casted
-* @size: Number indicating the type to be casted
+* @sze: Number indicating the type to be casted
 *
 * Return: Casted value of num
 */
-long int convert_size_unsgnd(unsigned long int num, int size)
+long int convert_size_unsgnd(unsigned long int num, int sze)
 {
-if (size == S_LONG)
+if (sze == S_LONG)
 return (num);
 else if (size == S_SHORT)
 return ((unsigned short)num);
