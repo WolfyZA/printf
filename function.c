a0e98ef631a271090e1feaf968fffaa624787365
WolfyZA <48529875+WolfyZA@users.noreply.github.com>
2023-04-24 17:22:18 +0200

    Update function.c

--- function.c
+++ function.c
@@ -6,10 +6,10 @@
  * print_char - Prints a char
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: Width
- * @precision: Precision specification
- * @size: Size specifier
+ * @flg:  Calculates active flags
+ * @wdth: Width
+ * @prec: Precision specification
+ * @sze: Size specifier
  * Return: Number of chars printed
  */
 int print_char(va_list types, char buffer[],
