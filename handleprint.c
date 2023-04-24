27963ff63688358e8102a69e8acbe199b927124e
WolfyZA <48529875+WolfyZA@users.noreply.github.com>
2023-04-24 18:50:50 +0200

    Update handleprint.c

--- handleprint.c
+++ handleprint.c
@@ -12,7 +12,7 @@
  * Return: 1 or 2;
  */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-	int flags, int width, int precision, int size)
+	int flg, int wdth, int prec, int sze)
 {
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
@@ -24,7 +24,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	};
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_types[i].fmt)
-			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+			return (fmt_types[i].fn(list, buffer, flg, wdth, prec, sze));
 
 	if (fmt_types[i].fmt == '\0')
 	{
@@ -33,7 +33,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		unknow_len += write(1, "%%", 1);
 		if (fmt[*ind - 1] == ' ')
 			unknow_len += write(1, " ", 1);
-		else if (width)
+		else if (wdth)
 		{
 			--(*ind);
 			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
